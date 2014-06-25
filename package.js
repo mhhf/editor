2e1ff54038f78813c2383f10ecab49b2df68f182
mhhf <mhhf@freenet.de>
2014-06-25 15:20:12 +0200

    yoyoyo commit

--- package.js
+++ package.js
@@ -10,10 +10,14 @@ Package.on_use(function (api) {
   
   api.add_files("lib/codemirror.js", "client");
   api.add_files("lib/codemirror.css", "client");
+  
+  api.add_files("themes/monokai.css", "client");
+  
   api.add_files("modes/markdown.js", "client");
   api.add_files("modes/javascript.js", "client");
   api.add_files("editorView.html", "client");
   api.add_files("editorView.js", "client");
+  
 
   if (api.export) 
     api.export('CodeMirror');
